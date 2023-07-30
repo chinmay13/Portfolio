1a94ba0ffa9b11d3fe823435b02a1318c6f98850
chinmay13 <chinmaybhate123@gmail.com>
2023-07-30 14:16:31 -0500

    added about, skills, work components

---
+++ portfolio-react-app/src/components/Skills.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import ReactImg from "../assets/React-icon.png";
+
+function Skills() {
+  return (
+    <div name="skills" className="bg-[#0a192F] h-screen w-full text-gray-300">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center items-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+            Experience
+          </p>
+          <p className="py-4"> These are the technologies I've worked with</p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="React Icon" />
+            <p>React JS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="React Icon" />
+            <p>React JS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="React Icon" />
+            <p>React JS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="React Icon" />
+            <p>React JS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="React Icon" />
+            <p>React JS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="React Icon" />
+            <p>React JS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="React Icon" />
+            <p>React JS</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="React Icon" />
+            <p>React JS</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
