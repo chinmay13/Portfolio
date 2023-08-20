2b2e1de3a5557c7f602e79f22e08413a5c940f1a
chinmay13 <chinmaybhate123@gmail.com>
2023-08-20 01:56:28 -0500

    Updated Theme, added description

--- portfolio-react-app/src/components/Common-style.jsx
+++ portfolio-react-app/src/components/Common-style.jsx
@@ -1,6 +1,10 @@
 const style = {
   background: `bg-[#0a192F]`,
   screenSize: `h-screen w-full`,
+  textColor: `text-[#64FFDA]`,
+  hoverTextColor: `hover:text-[#64FFDA]`,
+  underlineBorder: `border-b-4 border-[#64FFDA]`,
+  buttonStyle: `border-2  mx-auto flex items-center border-[#64FFDA] text-[#64FFDA]`,
 };
 
 export default style;
