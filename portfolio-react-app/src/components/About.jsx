4cd4502fd3cb05498be0e25cf7c56ac07deb095a
chinmay13 <chinmaybhate123@gmail.com>
2023-09-24 23:06:00 -0400

    Updated About Section

--- portfolio-react-app/src/components/About.jsx
+++ portfolio-react-app/src/components/About.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import style from "./Common-style";
+import pic1 from "../assets/Pic1.jpg";
+import pic2 from "../assets/pic2.jpg";
 
 function About() {
   return (
@@ -16,13 +18,36 @@ function About() {
           </div>
           <div></div>
         </div>
-        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-          <div className="sm:text-right text-4xl font-bold">
-            <p>Hi, I'm Chinmay, nice to meet you, Please take a look around</p>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-4 px-4">
+          <div className="sm:text-left text-1xl">
+            <p className="pb-4">
+              Welcome to my portfolio! I'm a dynamic IT professional with a
+              strong background in software development, specializing in Java,
+              Spring MVC, JavaScript, Python, React.js and database management
+              with Oracle and PostgreSQL. With three years of industry
+              experience, I've honed my skills in creating robust applications
+              and delivering innovative solutions. I thrive in fast-paced
+              environments and enjoy collaborating with cross-functional teams
+              to drive projects to successful completion.
+            </p>
+            <p>
+              I'm passionate about contributing to the success of innovative
+              organizations that value creativity, collaboration, and continuous
+              improvement. I'm open to new opportunities and look forward to
+              connecting with professionals who share a similar passion for
+              software development and technology.
+            </p>
           </div>
-          <div>
-            <p>This is where I will add my about</p>
+          <div className="self-center justify-center items-center align-middle">
+            <img
+              src={pic2}
+              className="max-h-[270px] max-w-[270px] mx-auto border-4 border-[#64FFDA] p-1"
+            />
           </div>
+          {/* <div className="justify-center items-center align-middle">
+            <img src={pic1} className="max-h-[200px] max-w-[200px] mx-auto" />
+          </div> */}
+          <div></div>
         </div>
       </div>
     </div>
